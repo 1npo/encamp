855953cb9969dce2455962571201610b1cf0c380
Nick O'Malley <nickpeteromalley@gmail.com>
2026-06-06 18:13:07 -0400

    added targets to `config.sh`

--- encamp/config.sh
+++ encamp/config.sh
@@ -3,6 +3,13 @@
 # Defines user settings
 #
 
+TARGETS=(
+    base
+    desktop
+    headless-workstation
+    lan-services
+)
+
 ARCH="x86_64"  # One of x86_64, arm64
 
 BIN_DIR="${HOME}/.local/bin"
@@ -14,4 +21,3 @@ mkdir -p "$BIN_DIR" "$STATE_DIR" "$SHARE_DIR" "$CACHE_DIR"
 CREATE_GENERAL_VENV="true"
 VENV_DIR="${HOME}/.venv"
 VENV_PYTHON="${VENV_DIR}/bin/python"
-
