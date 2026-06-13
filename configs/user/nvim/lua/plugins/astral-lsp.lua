6ee65f98f42f20ac39ee915091d26d95f29d8a1a
Nick O'Malley <nickpeteromalley@gmail.com>
2026-06-13 15:34:10 -0400

    added lazyvim config

---
+++ configs/user/nvim/lua/plugins/astral-lsp.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          mason = false,
+          autostart = false,
+        },
+        ruff = {},
+        ty = {},
+      },
+    },
+  },
+}
